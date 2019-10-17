c55ba66f2f0e503ca8f69cb9a221761b17903a3e
dadadadev <dadada80.dev@gmail.com>
2019-10-17 13:31:11 +0900

    apply syntax highlight

--- src/GistList.tsx
+++ src/GistList.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react';
+const {
+  useEffect,
+} = React;
+
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
+
+import * as Prism from 'prismjs';
+import 'prismjs/components/prism-haskell.min';
+import 'prismjs/components/prism-javascript.min';
+import 'prismjs/components/prism-typescript.min';
+import 'prismjs/themes/prism-tomorrow.css'
 
 const QUERY = gql`
   { 
@@ -25,20 +35,30 @@ export default () => {
   const {
     loading,
     error,
-    data
+    data,
   } = useQuery(QUERY);
+
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [loading, error, data])
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>something wrong</p>;
   
-  const gists = data.user.gists.nodes;
   return (
     <>
       <h4>Gists</h4>
-      {gists.map(gist => (
-        gist.files.map((data, i) => (
+      {data.user.gists.nodes.map(gist => (
+        gist.files.map(({
+          name,
+          extension,
+          text,
+        }, i) => (
           <React.Fragment key={i}>
-            <div>{data.name}</div>
+            <h4>{name}</h4>
+            <pre className={`language-${extension.slice(1)}`}> {/* remove '.' from extension */}
+              <code>{text}</code>
+            </pre>
           </React.Fragment>
         )
       )))}
